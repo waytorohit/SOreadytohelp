75c0802a8a612d609d71b8ff76c2721098761ae3
etelligens <ajay@etelligens.in>
2018-08-04 19:42:48 +0530

    Initial Commit

---
+++ SOreadytohelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOreadytohelp
+//
+//  Created by Etelligens-M on 04/08/18.
+//  Copyright © 2018 Etelligens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
