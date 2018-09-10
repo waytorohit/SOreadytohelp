5acc669fc40596948cb1a60968143e8c3fd0ebf6
rohit <rohit.sharma9760769526@gmail.com>
2018-09-10 19:12:06 +0530

    stack view
    simple sxample to use stackview

--- SOreadytohelp/ViewController.swift
+++ SOreadytohelp/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+  
+         let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_snake_case"
+        
+         guard let urlString = URL(string: jsonUrlString) else { return}
+        
+        URLSession.shared.dataTask(with: urlString) {
+            (data, response, error) in
+            
+            guard let data = data else {return}
+           
+            do {
+                
+                    let course = try
+                    JSONDecoder().decode(Course.self, from: data)
+                    print(course.name)
+                
+            } catch let jsonErr {
+                print("json serializing error:", jsonErr)
+            }
+            
+        }.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
+}
+
+// Create struct
+struct Course: Codable {
+    
+    let id: Int
+    let name: String
+    let link: String
+    let imageToUrl: String
+    
 
 }
 
